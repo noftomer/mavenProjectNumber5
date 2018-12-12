30e2e2f2d4d63ddc6c29ab5fc7c15eb348dff7b3
tomer <tomnof81@gmail.com>
2018-12-12 21:20:34 +0200

    maven

---
+++ src/test/java/TestNGForMaven.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class TestNGForMaven {
+  @Test
+  public void f1() {
+	  System.out.println("f1");
+  }
+  @Test
+  public void f2() {
+	  System.out.println("f2");
+  }
+  @Test
+  public void f3() {
+	  System.out.println("f3");
+  }
+}
